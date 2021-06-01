2dba7c8126ece81e826853fe18751e50ca1928cb
JStocke12 <56052177+JStocke12@users.noreply.github.com>
2021-06-01 19:48:50 -0400

    Added markov chain logic and command

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import discord
 import decouple
 from discord.ext import commands
+import random
 
 bot = commands.Bot(command_prefix="markov.")
 
@@ -11,5 +12,30 @@ async def on_ready():
 @bot.command()
 async def ping(ctx):
     await ctx.send("pong")
+
+@bot.command()
+async def gen(ctx):
+    pdict = {}
+
+    parsed = [i.split() for i in open("raw.txt","r",errors="ignore").read().split("\n\n")]
+
+    for l in parsed:
+        for i,j in zip(l,l[1:]):
+            if i in pdict:
+                pdict[i] = pdict[i] + [j]
+            else:
+                pdict[i] = [j]
+
+    out = ""
+
+    e = random.choice(list(pdict.keys()))
+
+    while e in pdict.keys():
+        out += e + " "
+        e = random.choice(pdict[e])
+
+    out += e
+
+    await ctx.send(out)
 
 bot.run(decouple.config('TOKEN'))
